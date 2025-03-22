1d4c149f7474d60047a61eaf630c6d9a1a2c6b26
Paula Drettas <pauladrettas@gmail.com>
2025-03-22 19:43:10 +0100

    FEAT(exec): implemented heredoc [still in progress]

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "../inc/minishell.h"
 #include "../inc/parsing.h"
 #include "../inc/signals.h"
+#include "execution.h"
 
 // To Test the Lexer (Tmporary function)
 void print_tokens(t_lst_token *tokens)
@@ -61,21 +62,70 @@ void command_loop(t_minishell *minishell)
 
 
 // The Main function 
-int	main(int argc, char **argv, char **env)
-{
-	t_minishell	*minishell;
+// int	main(int argc, char **argv, char **env)
+// {
+// 	t_minishell	*minishell;
 	
-	(void) argc;
-	(void) argv;
-	init_minishell(&minishell, env);
-	disable_echoctl(); // Disable echoing of control characters
-	// setup_signals_interactive();
+// 	(void) argc;
+// 	(void) argv;
+// 	init_minishell(&minishell, env);
+// 	disable_echoctl(); // Disable echoing of control characters
+// 	// setup_signals_interactive();
 
-	command_loop(minishell); // parsing, execution happens here
-	// printf("exit flag in main = %d\n", minishell->exit_requested); // for testing
+// 	command_loop(minishell); // parsing, execution happens here
+// 	// printf("exit flag in main = %d\n", minishell->exit_requested); // for testing
 
-	exit_shell(minishell); 
-	// printf("final exit code (in main) = %d\n", exit_code); // for testing
+// 	exit_shell(minishell); 
+// 	// printf("final exit code (in main) = %d\n", exit_code); // for testing
+
+// 	return (0);
+// }
+
+
+
+// --------------------- HEREDOC ---------------------------------------------
+int heredoc(char *delimiter) // TODO: call somewhere else / implement properly
+{
+	// (void) delimiter;
+	int heredoc;
+	char *line;
+
+	heredoc = open("viktoria1", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (heredoc == -1)
+	{
+		write(2, "minishell: ", 11); // bash
+		write(2, "viktoria1", 9);
+		write(2, ": No such file or directory\n", 28);
+		return (-1);
+	}
+
+	delimiter = ft_strjoin(delimiter, "\n"); // bc line has new line
+	while (1)
+	{
+		// printf("> "); //=========> [ '>', ' ' ]
+		// printf("\n");
+		write (1, "> ", 2);
+		line = get_next_line(0);
+		if (ft_strcmp(line, delimiter) == 0)
+		{
+			break;
+		}
+		write(heredoc, line, ft_strlen(line));
+		ft_free(line);
+		// printf("delimiter = -%s-\n", delimiter);
+		// printf("line = -%s-\n", line);
+	}
+	ft_free(delimiter);
+
+	close(heredoc);
+	unlink("viktoria1");
+	
+	return (1);
+}
+
+int main ()
+{
+	heredoc("n");
 
 	return (0);
 }
